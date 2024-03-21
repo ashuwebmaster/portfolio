d6cb550083e327906b56efd6ee6e15ff615ba755
Ashutosh Joshi <kumar.ashutosh@hestabit.in>
2024-03-21 18:40:16 +0530

    First commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,14 @@
+export const personalData = {
+  name: "Ashutosh Joshi",
+  profile: '/profile.jpeg',
+  designation: "Software Developer",
+  description: "My name is Ashutosh Joshi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'ashu.joshi.dev@gmail.com',
+  phone: '+918744011645',
+  address: 'Noida Sector 62, B Block market, Uttar Pradesh.',
+  github: 'https://github.com/ashuwebmaster/',
+  linkedIn: 'https://www.linkedin.com/in/joshiaashu/',
+  leetcode: "https://leetcode.com/ashujoshi/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
